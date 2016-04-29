804cb60dc0db0a0d96da2ee99e41562817f32850
bovenson <szhkai@qq.com>
2016-04-29 20:25:40 +0800

    完成导航栏

--- wildteam/settings.py
+++ wildteam/settings.py
@@ -101,7 +101,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'serverstatic/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'serverstatic/')
 # -----------------------------------------------------
 
 # Authentication backends
