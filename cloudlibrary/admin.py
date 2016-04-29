63446efeb91564a54cd55a59999b4a3a564d545e
bovenson <szhkai@qq.com>
2016-04-29 19:10:40 +0800

    重构前台界面前

--- cloudlibrary/admin.py
+++ cloudlibrary/admin.py
@@ -1,9 +1,10 @@
 # codding: utf-8
 from django.contrib import admin
-from cloudlibrary.models import WildUser, WildBook
+from cloudlibrary.models import WildUser, WildBook, WildBookHistory
 
 # Register your models here.
 
 
 admin.site.register(WildUser)
 admin.site.register(WildBook)
+admin.site.register(WildBookHistory)
--- cloudlibrary/models.py
+++ cloudlibrary/models.py
@@ -55,4 +55,7 @@ class WildBookHistory(models.Model):
     description = models.CharField(max_length=200, default="")
     owner = models.IntegerField(null=True)
     time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
     pass
