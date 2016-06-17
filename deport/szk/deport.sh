1c343041076d6364d2b735f6d37f3679d1c5bd80
bovenson <szhkai@qq.com>
2016-06-17 18:55:02 +0800

    修改deport.sh换行格式

--- deport/szk/deport.sh
+++ deport/szk/deport.sh
@@ -1,1 +1,3 @@
-#!/bin/bashsudo rm /etc/nginx/sites-enabled/wildbook_nginx.confsudo ln -s /home/ubuntu/project/wildbook/deport/szk/wildbook_nginx.conf /etc/nginx/sites-enabled/wildbook_nginx.confsudo service nginx restart
+#!/bin/bash
+sudo rm /etc/nginx/sites-enabled/wildbook_nginx.conf
+sudo ln -s /home/ubuntu/project/wildbook/deport/szk/wildbook_nginx.conf /etc/nginx/sites-enabled/wildbook_nginx.confsudo service nginx restart
